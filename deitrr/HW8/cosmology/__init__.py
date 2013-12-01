4caaafb78c571e0d53db58bc0070bfd9dc7fcba2
deitrr <deitrr@uw.edu>
2013-11-30 23:17:54 -0800

    commit hw 8 d00d

---
+++ deitrr/HW8/cosmology/__init__.py
@@ -0,0 +1,11 @@
+"""Cosmology tools
+
+This modules helps you measure the universe. 
+
+Classes:
+- Cosmology : class whose methods are cosmological parameters/distances
+
+"""
+# We'll import the Cosmology class using
+# a "relative import":
+from .cosmology import Cosmology
