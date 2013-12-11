75062fcbda27fa1feb80776b371363ddad663b64
suberlak <suberlak@uw.edu>
2013-12-11 12:54:26 -0800

    three hw

---
+++ suberlak/HW8/cosmology/__init__.py
@@ -0,0 +1,10 @@
+"""Cosmology tools
+
+A module with Cosmology() class, which computes various distances in the Universe.
+
+Classes: Cosmology()
+
+"""
+# We'll import the Cosmology class using
+# a "relative import":
+from .cosmology import Cosmology
